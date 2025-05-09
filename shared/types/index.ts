50c2e51c1450bed1e4fc5d762164dd62b8b851ae
Slava Haikin <vyachowski@gmail.com>
2025-05-09 17:22:54 +0700

    refactor: add common shared types

---
+++ shared/types/index.ts
@@ -0,0 +1,18 @@
+export enum MealTime {
+	STARTER = "STARTER",
+	BREAKFAST = "BREAKFAST",
+	BRUNCH = "BRUNCH",
+	LUNCH = "LUNCH",
+	SNACK = "SNACK",
+	DINNER = "DINNER",
+}
+
+export enum WeekDays {
+	MONDAY = 0,
+	TUESDAY = 1,
+	WEDNESDAY = 2,
+	THURSDAY = 3,
+	FRIDAY = 4,
+	SATURDAY = 5,
+	SUNDAY = 6,
+}
